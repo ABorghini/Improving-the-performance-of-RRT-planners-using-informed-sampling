e2329203fb4944c9bb6ac3113a0df7fdc341fde0
Alessia Borghini <80713403+ABorghini@users.noreply.github.com>
2023-02-16 17:31:18 +0100

    Add files via upload

--- utils.py
+++ utils.py
@@ -11,10 +11,8 @@ from scipy.spatial.transform import Rotation as Rot
 
 def plot(algo, iteration, x_center=None, c_best=np.inf, dist=None, theta=None):
 
-    # print("V",len(rrt.V))
     plt.cla()
-    # if iteration == 0:
-    #     rrt.in_plot = []
+
     plot_grid(f"{algo.name}, N = {str(algo.iter_max)}", algo)
 
     lines = [[(n.x, n.y),(n.parent.x, n.parent.y)] for n in algo.V if n.parent]
@@ -50,8 +48,12 @@ def plot_grid(name, rrt):
             )
         )
 
-    plt.plot(rrt.x_start.x, rrt.x_start.y, "bs", linewidth=3)
-    plt.plot(rrt.x_goal.x, rrt.x_goal.y, "rs", linewidth=3)
+    if rrt.name == "RRTK*":
+        plt.plot(rrt.x_start.node[0], rrt.x_start.node[1], "bs", linewidth=3)
+        plt.plot(rrt.x_goal.node[0], rrt.x_goal.node[1], "rs", linewidth=3)
+    else:
+        plt.plot(rrt.x_start.x, rrt.x_start.y, "bs", linewidth=3)
+        plt.plot(rrt.x_goal.x, rrt.x_goal.y, "rs", linewidth=3)
 
     plt.title(name)
     plt.axis("equal")
@@ -71,3 +73,37 @@ def draw_ellipse(x_center, c_best, dist, theta):
     py = np.array(fx[1, :] + cy).flatten()
     plt.plot(cx, cy, ".b")
     plt.plot(px, py, linestyle='--', color='darkorange', linewidth=2)
+
+
+def plot_kino(rrtk, x_center=None, c_best=np.inf, dist=None, theta=None):
+    step = 1.0
+    path = rrtk.path
+    path.reverse()
+
+    plt.cla()
+
+    print("plot_grid")
+    plot_grid(f"{rrtk.name}, N = {str(rrtk.iter_max)}", rrtk)
+    states_list = []
+    for idx, node in enumerate(path):
+        print(idx)
+        if idx == len(path)-1:
+            break
+        t_goal = rrtk.eval_arrival_time(node, path[idx+1])
+        times = np.arange(0, t_goal/step)*step
+        states = rrtk.states_eq(t_goal, node, path[idx+1])
+        
+        states_list.extend([states.subs({rrtk.t:t})[0:2] for t in times])
+
+    print(states_list)
+    x, y = zip(*states_list)
+    if len(path) > 2:
+        x_, y_, _, _ = zip(*path[1:-1])
+    else:
+        x_ = []
+        y_ = []
+    plt.plot(x,y, color='c')
+    plt.plot(x_,y_, color='g', marker='o', linestyle='', markersize=4)
+    plt.show()
+    plt.pause(0.01)
+    return
